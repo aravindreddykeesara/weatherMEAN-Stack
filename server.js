119f4b07ec57c50a11691429d516be508738b349
Aravind Reddy Keesara <31300215+aravindreddykeesara@users.noreply.github.com>
2018-07-27 15:13:30 -0400

    added heroku support

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
-const app = express();
-const api = require('./server/routes/api');
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var path = require('path');
+var http = require('http');
+var app = express();
+var api = require('./server/routes/api');
+
 
 //parsers 
 app.use(bodyParser.json());
@@ -12,19 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.options('*', cors());
+
+app.use(cors());
 //set api routes
 app.use('/api',api);
-
 //return other routes to angular index file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/weatherMean/index.html'));
+app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirname, 'dist','index.html'));
 });
-
 //set port
-const port = process.env.PORT || '3000';
+var port = process.env.PORT || 5000;
 app.set('port', port);
-
 //create the http server
 const server = http.createServer(app);
-
-server.listen(port, ()=> console.log(`running on localhost:${port}`) );
+server.listen(port, () => console.log(`running on localhost:${port}`));
