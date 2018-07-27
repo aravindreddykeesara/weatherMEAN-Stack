a0eeb69e84870bc1a8c488ad9cc0bc7d057b1eb7
Aravind Reddy Keesara <31300215+aravindreddykeesara@users.noreply.github.com>
2018-07-27 15:10:53 -0400

    api routes added

--- server/routes/api.js
+++ server/routes/api.js
@@ -30,20 +30,80 @@ userfav.find({}).exec(function (err, posts) {
 });
 
 
-router.get('/addUser', function (req, res) {
+
+
+router.post('/addUser', function (req, res) {
   console.log('adding user to db');
   var newUser = new userfav();
   newUser.userEmail = req.body.userEmail;
   newUser.password = req.body.password;
   newUser.save(function(err,addedUser){
       if(err){
-          console.log('error adding user to db');
+          res.json({
+            "userEmail":null
+          });
       }else{
           res.json(addedUser);
       }
   });
   
 });
+
+
+router.get('/loginUser', function (req, res) {
+
+  console.log('checking 2 user in db' + JSON.parse(req.query.updates[0]).value);
+
+  var loginUser = new userfav();
+  loginUser.userEmail = req.params.userEmail;
+  loginUser.password = req.body.password;
+  userfav.find({
+            "userEmail":JSON.parse(req.query.updates[0]).value,
+            "password": JSON.parse(req.query.updates[1]).value
+            }).exec(function (err, posts) {
+
+    if (err) {
+      console.log('erreor getting userlogdata');
+    } else {
+      res.json(posts);
+      console.log('user exists data' + JSON.stringify(posts));
+    }
+  })
+
+
+});
+
+
+
+router.post('/UpdateFavorites', function (req, res) {
+
+
+    console.log("json for updating locations" + JSON.stringify(req.body));
+
+  var updateLocation =  new userfav();
+  updateLocation.favcities = Array.from(req.body["favcitiesvalue"]);
+
+  userfav.update({"userEmail": req.body["userEmailValue"] },
+    {$set:{"favcities": updateLocation.favcities}}
+).exec(function (err, posts) {
+
+  if (err) {
+    console.log('erreor upadting locations');
+  } else {
+    res.json(posts);
+    console.log('updated locations' + JSON.stringify(posts));
+  }
+})
+
+});
+
+
+
+
+
+
+
+
 
 
 
